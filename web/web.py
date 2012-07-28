3c87864e5bcb345af742ca60a403ca91a716eb18
Tom Wardill <tom@howrandom.net>
2012-07-28 23:39:48 +0100

    Accept payload, save photo, save to mongo

--- web/web.py
+++ web/web.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+from pymongo import Connection
+import os
 
 app = Flask(__name__)
 
@@ -9,12 +11,30 @@ def main():
 @app.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def post(post_id):
     if request.method == 'POST':
-        print request.form
-        print request.files
-        return "POST"
+        try: 
+            
+            doc = {
+                'author': request.form['author'],
+                'location': request.form['location'],
+                'commit_message': request.form['commit_message'],
+                'photo': request.form['photo']
+            }
+            connection = Connection()
+            db = connection.developerhealth
+            db.payloads.insert(doc)
+            
+            
+            path = os.path.dirname(os.path.abspath(__file__))
+            filename = request.form['photo']
+            filename = os.path.join(path, filename)
+            f = request.files['photofile']
+            f.save(filename)
+            return "SUCCESS"
+        except Exception, ex:
+            print ex
+            return "argh"
     else:
         return "post: " + str(post_id)
     
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    app.run(host="0.0.0.0")
